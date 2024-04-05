6c821f7445bda9fca6af71504e0065a490d425f9
Renebert Alcarde <renebertalcarde76@gmail.com>
2024-04-05 13:00:50 +0800

    update

--- CICDTEST/Controllers/HomeController.cs
+++ CICDTEST/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication4.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "LogixSys CI/CD Test";
 
             return View();
         }
